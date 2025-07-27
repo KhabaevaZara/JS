636d898a42944e622bf6fae94bcaee125b656371
ZaraHabaeva <ZaraKhabaeva52@gmail.com>
2025-07-27 16:22:52 +0300

    1

--- backend/core/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import permissions
-
-class UserAdminPermission(permissions.BasePermission):
-
-    def has_object_permission(self, request, view, obj):
-        if obj.user == request.user or request.user.is_superuser:
-            return True
-        return False
